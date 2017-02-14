92c332dca5182a19f157a82ef09813f57973446e
latelee <latelee@163.com>
2017-02-14 18:01:20 +0800

    阴历转阴历：去掉闰月标志，直接使用闰月月份判断。 添加新的计算方式。
    Signed-off-by: latelee <latelee@163.com>

--- project/yl2yl/get_nl.py
+++ project/yl2yl/get_nl.py
@@ -26,7 +26,7 @@
 # 2、遍历列表，获取“X月”那一行，前一行即为上月月份日数 －－所以代码中需要进行减1操作
 # 3、由于农历跨年，所以需要全局变量，并且只能在“正月”才能统计上一年日历数据
 # 4、闰月根据关键字判断，由于是计算“上月”，所以闰月需要与相同的月份交换
-# 5、将需要信息依次保存到monthlist列表。再转换成二进制。最终生成的格式如下：
+# 5、将需要信息依次保存到yearlist列表。再转换成二进制。最终生成的格式如下：
 #      ~21       20~18   17~14    13         12         11~0
 #   农历总日数  立春日  闰几月 闰月日数  是否闰月  1~12月份农历日数
 #
@@ -38,6 +38,7 @@ import os
 import sys
 import re
 import datetime
+import time
 
 import urllib.request
 
@@ -54,16 +55,16 @@ y, m, d = 0, 0, 0 # 数字形式
 leapmonth_m, leapmonth_d, leapmonth_is = 0, 0, 0
 lichun_d = 0
 chunjie_m, chunjie_d = 0, 0
+days_yuandan = 0
 day_cnt = 1
 y_nl = 0
 leap_flag = 0
-monthlist=[0]*20 # 第一个即为年份
+yearlist=[0]*20 # 第一个即为年份
 
 binary_data = 0
 
 f=None
 line_cnt=0
-
 error_f = None
 
 # 网页第一行数据即为“XX月”，需要返回上一网页获取最后一行
@@ -89,11 +90,11 @@ def get_last_date(url, year):
 
     return ret
 
-def get_nongli(url, year1, year2):
+def open_file(file, array):
     global f
     global error_f
 
-    f = open("nongli.txt", "w")
+    f = open(file, "w")
     error_f = open("error.txt", "w")
 
     test  = "#####################################################################################\n"
@@ -101,18 +102,39 @@ def get_nongli(url, year1, year2):
     test += "# powered by Late Lee, http://www.latelee.org \n"
     test += "# %s \n" % (datetime.datetime.now())
     test += "#阴历数据 每个元素的存储格式如下： \n"
-    test += "#    ~21       20~18   17~14    13         12         11~0  \n"
-    test += "# 农历总日数  立春日  闰几月 闰月日数  是否闰月  1~12月份农历日数  \n"
+    test += "#   16~13    12          11~0  \n"
+    test += "#  闰几月 闰月日数  1~12月份农历日数  \n"
     test += "# 注：1、bit0表示农历1月份日数，为1表示30天，为0表示29天。bit1表示农历2月份日数，依次类推。 \n"
-    test += "#    2、bit12表示该年是否有闰月，bit13表示闰月日数，1为30天，0为29天。bit17~bit14表示第几月是闰月 \n"
+    test += "#     2、bit12表示闰月日数，1为30天，0为29天。bit17~bit14表示第几月是闰月(注：为0表示该年无闰月) \n"
     test += "# 数据来源: http://data.weather.gov.hk/gts/time/conversion1_text_c.htm 由Jim Kent编写python爬虫强力分析\n"
     test += "#####################################################################################\n"
 
-    test += "my_g_lunar_month_day = [\n\t"
+    test += "%s = [\n\t" % (array)
     f.write(test)
 
+def get_nongli(url, year1, year2):
+    global f
+    global error_f
+
+    open_file("nongli.txt", "my_g_lunar_month_day")
+    
     for year in range(year1, year2+1):
         aaaa(url, year)
+
+    test = "]\n"
+    f.write(test)
+    f.close()
+
+    error_f.close()
+
+def get_nongli2(url, year1, year2):
+    global f
+    global error_f
+    
+    open_file("nongli111.txt", "my_g_lunar_day")
+    
+    for year in range(year1, year2+1):
+        bbb(url, year)
 
     test = "]\n"
     f.write(test)
@@ -127,7 +149,7 @@ def aaaa(url, year):
     global day_cnt
     global y, m, d
     global yy_nl
-    global monthlist
+    global yearlist
     global binary_data
     global f
     global line_cnt
@@ -184,80 +206,81 @@ def aaaa(url, year):
         if yue == "正月":
             day_cnt = monthdict[tmp[i-1].split()[1]] # 正月才能确认腊月日数
             yy_nl = y - 1
-            monthlist[0] = yy_nl
-            monthlist[mydict[yue[0:]]-1] = day_cnt
+            yearlist[0] = yy_nl
+            yearlist[mydict[yue[0:]]-1] = day_cnt
             # 由于计算月份日期是在次月方知，所以闰月与非闰月是相反的 (如得到的闰五月日期数实为五月日期数)，这里调换
             if leap_flag == 1:
                 #print("!!! 有闰月 %d %d " % (leapmonth_m, leapmonth_d))
                 changed_tmp = leapmonth_d
-                leapmonth_d = monthlist[leapmonth_m]
-                monthlist[leapmonth_m] = changed_tmp
+                leapmonth_d = yearlist[leapmonth_m]
+                yearlist[leapmonth_m] = changed_tmp
                 leap_flag = 0
             # 消掉全局变量，全年没有闰月时，清零
             else:
                 leapmonth_m, leapmonth_d, leapmonth_is = 0, 0, 0
 
             i = 13
-            monthlist[i] = leapmonth_is
-            i+=1
-            monthlist[i] = leapmonth_d
-            i+=1
-            monthlist[i] = leapmonth_m
-            i+=1
-            #monthlist[i] = chunjie_d
+            #yearlist[i] = leapmonth_is
             #i+=1
-            #monthlist[i] = chunjie_m
-            #i+=1
-            monthlist[i] = lichun_d
+            yearlist[i] = leapmonth_d
             i+=1
+            yearlist[i] = leapmonth_m
+            i+=1
+            #yearlist[i] = chunjie_d
+            #i+=1
+            #yearlist[i] = chunjie_m
+            #i+=1
+            #yearlist[i] = lichun_d
+            #i+=1
 
             #############################################################
             #　转换成二进制
             for j in range(1, 13):
-                k = monthlist[j]
+                k = yearlist[j]
                 if k == 1:
                     binary_data |= k << (j-1)# 月份
                     total_date += 30
                 else:
                     total_date += 29
-            if leapmonth_is == 1:
+            if leapmonth_m:
                 if leapmonth_d:
                     total_date += 30
                 else:
                     total_date += 29
             j = 13
-            binary_data |= monthlist[j] << 12
+            #binary_data |= yearlist[j] << 12
+            #j+=1
+            binary_data |= yearlist[j] << 12
             j+=1
-            binary_data |= monthlist[j] << 13
-            j+=1
-            binary_data |= monthlist[j] << 14
-            j+=1
-            binary_data |= monthlist[j] << 18
+            binary_data |= yearlist[j] << 13
+            #j+=1
+            #binary_data |= yearlist[j] << 17
 
-            binary_data |= total_date << 21 # 该年农历日期总日数
+            #binary_data |= total_date << 21 # 该年农历日期总日数
 
             ######################################################
             line_cnt += 1
-            text = str(hex(binary_data)) + ", "
+            #text = str(hex(binary_data)) + ", "
+            text = "0x%05x, " % binary_data # 格式化好一点，前补0
             if line_cnt % 10 == 0:
                 text += "# %d ~ %d \n\t" % (yy_nl-10+1, yy_nl)
 
             f.write(text)
             ######################################################
             #j+=1
-            #binary_data |= monthlist[j] << 24
+            #binary_data |= yearlist[j] << 24
             #j+=1
-            #binary_data |= monthlist[j] << 25
+            #binary_data |= yearlist[j] << 25
             #print("%d月 有%d天" % (mydict[yue[0:]]-1, day_cnt))
             print("农历%d年日历小结　" % (yy_nl))
             print("  --春节：%d年%d月%d日 农历总日期：%d" % (yy_nl, chunjie_m, chunjie_d, total_date))
             print("  --立春：2月%d日" % (lichun_d))
-            if leapmonth_is != 0:
+            if leapmonth_m != 0:
                 print("  --闰%d月 (大小月？ %d)" % (leapmonth_m, leapmonth_d))
-            print("数据: ", monthlist)
+            print("数据: ", yearlist)
 
             print("生成二进制数据：%s" % str(hex(binary_data)))
-            monthlist=[0]*20
+            yearlist=[0]*20
             binary_data = 0
             #print("-------------------\n计算农历%d年日历" % (y))
             chunjie_m = m
@@ -274,7 +297,7 @@ def aaaa(url, year):
                     error_text = "error data found: %s source: %s\n" %(new_url, tmpp)
                     error_f.write(error_text)
 
-                    # 特殊处理。。。
+                    # 下面这些是格式错误的，要特殊处理。。。
                     if gl == '2053年12月10日': # 前一天：农历十月三十 星期二
                         day_cnt = 1
                     if gl == '2056年3月16日':  # 前一天：农历正月三十 星期三
@@ -292,15 +315,83 @@ def aaaa(url, year):
                 leapmonth_d = day_cnt
                 #print("闰%d月 有%d天 %d %d %d" % (mydict[yue[1:]], day_cnt, leapmonth_is, leapmonth_d, leapmonth_m))
             else:
-                monthlist[mydict[yue[0:]]-1] = day_cnt
+                yearlist[mydict[yue[0:]]-1] = day_cnt
                 #print("%d月 有%d天" % (mydict[yue[0:]]-1, day_cnt))
+    #time.sleep(1) # 不要那么快访问服务器，以防被拒绝
 
+
+need_write = 0
+
+def bbb(url, year):
+    global lichun_d
+    global chunjie_m, chunjie_d
+    global days_yuandan
+    global day_cnt
+    global y, m, d
+    global yy_nl
+    global yearlist
+    global binary_data
+    global f
+    global line_cnt
+    global need_write
+
+    new_url = url + "T%dc.txt" % year
+    request = urllib.request.Request(new_url)
+    response = urllib.request.urlopen(request)
+    src = response.read().decode('big5')
+    print("parsing for %s" % new_url)
+
+    tmp = src.split('\n')
+
+    # 第三行才是真正的数据
+    start_row = 3
+    total_date = 0
+    for i in range(start_row, len(tmp)): # len(tmp)-3
+        tmpp = tmp[i].split() # 格式：1903年01月01日 初三 星期四 [节气]
+        if (len(tmpp)) == 0:
+            break;
+        gl = tmpp[0] # 日期，形式不固定，如：1903年01月01日、2011年12月1日
+        gl_tmp = re.findall(r'\d+', gl) # 正则表示式获取日期数字，如 1903年01月01日 --> ['1903', '01', '01']
+        y, m, d = int(gl_tmp[0]), int(gl_tmp[1]), int(gl_tmp[2])
+        #print(y, m, d)
+        if len(tmpp) == 4: # 长度为4表明有节气
+            jieqi = tmpp[3]
+            #print(jieqi, end='')
+            if (jieqi == "立春"):
+                lichun_d = d
+                need_write += 1
+        yue = tmpp[1]
+        #print(gl, yue)
+        if yue == "正月":
+            chunjie_m = m
+            chunjie_d = d
+            need_write += 1
+            days_yuandan = i - start_row
+
+        # 当正月&&立春时再计算
+        if need_write == 2:
+            need_write = 0
+            print("%d年：正月：%d月%d日 立春：2月%d日 离该年元旦有%d天" % (year, chunjie_m, chunjie_d, lichun_d, days_yuandan))
+            binary_data |= chunjie_d << 0
+            binary_data |= chunjie_m << 5
+            binary_data |= days_yuandan << 7
+            #text = str(hex(binary_data))
+            text = "0x%04x, " % binary_data # 格式化好一点，前补0
+            bin_text = str(bin(binary_data))
+            print("二进制数据: %s %s" % (text, bin_text))
+            line_cnt += 1
+            if line_cnt % 10 == 0:
+                text += "# %d ~ %d \n\t" % (year-10+1, year)
+
+            f.write(text)
+
+            binary_data = 0
+    #time.sleep(2) # 不要那么快访问服务器，以防被拒绝
 
 def init_python_env():
     if sys.version_info.major == 2:
        reload(sys)
        sys.setdefaultencoding('utf8')
-
 
 #### main
 if __name__ == '__main__':
@@ -308,3 +399,4 @@ if __name__ == '__main__':
 
     # add here
     get_nongli("http://data.weather.gov.hk/gts/time/calendar/text/", 1901, 2100) # 2100  1901
+    #get_nongli2("http://data.weather.gov.hk/gts/time/calendar/text/", 1901, 2100) # 2100  1901
--- project/yl2yl/gl2nl_my.py
+++ project/yl2yl/gl2nl_my.py
@@ -3,54 +3,81 @@
 # Author: Late Lee
 # 2017.2.11
 
+#阴历数据 每个元素的存储格式如下：
+#   26~25   24~20   19~17    16~13    12          11~0
+#   春节月  春节日  立春日  闰几月 闰月日数  1~12月份农历日数
+
+#  27~25   24~23   22~18    17~14   13        12              11~0
+#  立春日  春节月  春节日 闰几月 闰月日数  是否闰月    1~12月份农历日数
+
+#    ~13          12~7         6~5    4~0
+# 农历有多少天 离元旦多少天  春节月  春节日
+
+
+
 #####################################################################################
 # 1901~2100年阴历数据表
-# powered by Late Lee, http://www.latelee.org 
-# 2017-02-12 23:02:06.546446 
-#阴历数据 每个元素的存储格式如下： 
-#    ~21       20~18   17~14    13         12         11~0  
-# 农历总日数  立春日  闰几月 闰月日数  是否闰月  1~12月份农历日数  
-# 注：1、bit0表示农历1月份日数，为1表示30天，为0表示29天。bit1表示农历2月份日数，依次类推。 
-#    2、bit12表示该年是否有闰月，bit13表示闰月日数，1为30天，0为29天。bit17~bit14表示第几月是闰月 
+# powered by Late Lee, http://www.latelee.org
+# 2017-02-12 23:02:06.546446
+#阴历数据 每个元素的存储格式如下：
+#  16~13    12         11~0
+# 闰几月 闰月日数 1~12月份农历日数
+# 注：1、bit0表示农历1月份日数，为1表示30天，为0表示29天。bit1表示农历2月份日数，依次类推。
+#     2、bit12表示闰月日数，1为30天，0为29天。bit17~bit14表示第几月是闰月(注：为0表示该年无闰月)
 # 数据来源: http://data.weather.gov.hk/gts/time/conversion1_text_c.htm 由Jim Kent编写python爬虫强力分析
 #####################################################################################
 my_g_lunar_month_day = [
-	0x2c540752, 0x2c740ea5, 0x2ff55b2a, 0x2c50064b, 0x2c700a9b, 0x30153aa6, 0x2c54056a, 0x2c740b59, 0x30149baa, 0x2c540752, # 1901 ~ 1910 
-	0x30159da5, 0x2c500b25, 0x2c500a4b, 0x30157a4b, 0x2c5402ad, 0x2c74056b, 0x301095b5, 0x2c700da9, 0x3015fe92, 0x2c500e92, # 1911 ~ 1920 
-	0x2c500d25, 0x30155d2d, 0x2c540a56, 0x2c5402b6, 0x30313ad5, 0x2c5006d4, 0x2c740ea9, 0x30109f4a, 0x2c500e92, 0x2ff596a6, # 1921 ~ 1930 
-	0x2c54052b, 0x2c740a57, 0x30117956, 0x2c700b5a, 0x2c5406d4, 0x3010f761, 0x2c500749, 0x3015fb13, 0x2c540a93, 0x2c54052b, # 1931 ~ 1940 
-	0x3011b51b, 0x2c740aad, 0x2c54056a, 0x30313da5, 0x2c500ba4, 0x2c500b49, 0x30149d4b, 0x2c540a95, 0x3011daad, 0x2c500536, # 1941 ~ 1950 
-	0x2c700aad, 0x30117aca, 0x2c5005b2, 0x2c700da5, 0x3014fea2, 0x2c540d4a, 0x2ff21595, 0x2c700a97, 0x2c500556, 0x30119575, # 1951 ~ 1960 
-	0x2c700ad5, 0x2c5006d2, 0x30151755, 0x2c740ea5, 0x2c30064a, 0x3010d64f, 0x2c700a9b, 0x3011dada, 0x2c50056a, 0x2c700b69, # 1961 ~ 1970 
-	0x30155bb2, 0x2c540b52, 0x2c500b25, 0x30111b2b, 0x2c500a4b, 0x30121aab, 0x2c5002ad, 0x2c70056d, 0x3015b5a9, 0x2c700da9, # 1971 ~ 1980 
-	0x2c500d92, 0x30111e95, 0x2c500d25, 0x30129e4d, 0x2c500a56, 0x2c5002b6, 0x301192f5, 0x2c7006d5, 0x2c700ea9, 0x30115f52, # 1981 ~ 1990 
-	0x2c500e92, 0x2c500d26, 0x2ff0d52e, 0x2c700a57, 0x30121ad6, 0x2c50035a, 0x2c7006d5, 0x30115b69, 0x2c500749, 0x2c500693, # 1991 ~ 2000 
-	0x30111a9b, 0x2c50052b, 0x2c700a5b, 0x30109aae, 0x2c50056a, 0x3031ddd5, 0x2c500ba4, 0x2c500b49, 0x30115d53, 0x2c500a95, # 2001 ~ 2010 
-	0x2c50052d, 0x3011155d, 0x2c700ab5, 0x30125baa, 0x2c5005d2, 0x2c700da5, 0x3011be8a, 0x2c500d4a, 0x2c500c95, 0x300d1a9e, # 2011 ~ 2020 
-	0x2c4c0556, 0x2c700ab5, 0x30109ada, 0x2c5006d2, 0x30119765, 0x2c500725, 0x2c50064b, 0x300d5657, 0x2c6c0cab, 0x2c50055a, # 2021 ~ 2030 
-	0x3010d56e, 0x2c700b69, 0x3012df52, 0x2c500b52, 0x2c500b25, 0x300dbd0b, 0x2c500a4b, 0x2c5004ab, 0x301152bb, 0x2c7005ad, # 2031 ~ 2040 
-	0x2c6c0b6a, 0x30109daa, 0x2c500d92, 0x300ddea5, 0x2c500d25, 0x2c500a55, 0x30117a4d, 0x2c5004b6, 0x2c6c05b5, 0x3010f6d2, # 2041 ~ 2050 
-	0x2c700ec9, 0x300e1f92, 0x2c4c0e92, 0x2c4c0d26, 0x2ff1b516, 0x2c6c0a57, 0x2c4c0556, 0x30113365, 0x2c700755, 0x2c500749, # 2051 ~ 2060 
-	0x300cd74b, 0x2c4c0693, 0x3011daab, 0x2c4c052b, 0x2c6c0a5b, 0x30115aba, 0x2c50056a, 0x2c700b65, 0x300d1baa, 0x2c4c0b4a, # 2061 ~ 2070 
-	0x30121d95, 0x2c4c0a95, 0x2c4c052d, 0x3011956d, 0x2c700ab5, 0x2c5005aa, 0x300d15d5, 0x2c6c0da5, 0x2c500d4a, 0x300cde4d, # 2071 ~ 2080 
-	0x2c4c0c96, 0x300ddcce, 0x2c500556, 0x2c700ab5, 0x300d7ad2, 0x2c4c06d2, 0x2c6c0ea5, 0x2fed172a, 0x2c4c068b, 0x300e1697, # 2081 ~ 2090 
-	0x2c5004ab, 0x2c70055b, 0x300db556, 0x2c6c0b6a, 0x2c4c0752, 0x300d1b95, 0x2c4c0b45, 0x2c4c0a8b, 0x30109a4f, ]
+	0x00752, 0x00ea5, 0x0ab2a, 0x0064b, 0x00a9b, 0x09aa6, 0x0056a, 0x00b59, 0x04baa, 0x00752, # 1901 ~ 1910
+	0x0cda5, 0x00b25, 0x00a4b, 0x0ba4b, 0x002ad, 0x0056b, 0x045b5, 0x00da9, 0x0fe92, 0x00e92, # 1911 ~ 1920
+	0x00d25, 0x0ad2d, 0x00a56, 0x002b6, 0x09ad5, 0x006d4, 0x00ea9, 0x04f4a, 0x00e92, 0x0c6a6, # 1921 ~ 1930
+	0x0052b, 0x00a57, 0x0b956, 0x00b5a, 0x006d4, 0x07761, 0x00749, 0x0fb13, 0x00a93, 0x0052b, # 1931 ~ 1940
+	0x0d51b, 0x00aad, 0x0056a, 0x09da5, 0x00ba4, 0x00b49, 0x04d4b, 0x00a95, 0x0eaad, 0x00536, # 1941 ~ 1950
+	0x00aad, 0x0baca, 0x005b2, 0x00da5, 0x07ea2, 0x00d4a, 0x10595, 0x00a97, 0x00556, 0x0c575, # 1951 ~ 1960
+	0x00ad5, 0x006d2, 0x08755, 0x00ea5, 0x0064a, 0x0664f, 0x00a9b, 0x0eada, 0x0056a, 0x00b69, # 1961 ~ 1970
+	0x0abb2, 0x00b52, 0x00b25, 0x08b2b, 0x00a4b, 0x10aab, 0x002ad, 0x0056d, 0x0d5a9, 0x00da9, # 1971 ~ 1980
+	0x00d92, 0x08e95, 0x00d25, 0x14e4d, 0x00a56, 0x002b6, 0x0c2f5, 0x006d5, 0x00ea9, 0x0af52, # 1981 ~ 1990
+	0x00e92, 0x00d26, 0x0652e, 0x00a57, 0x10ad6, 0x0035a, 0x006d5, 0x0ab69, 0x00749, 0x00693, # 1991 ~ 2000
+	0x08a9b, 0x0052b, 0x00a5b, 0x04aae, 0x0056a, 0x0edd5, 0x00ba4, 0x00b49, 0x0ad53, 0x00a95, # 2001 ~ 2010
+	0x0052d, 0x0855d, 0x00ab5, 0x12baa, 0x005d2, 0x00da5, 0x0de8a, 0x00d4a, 0x00c95, 0x08a9e, # 2011 ~ 2020
+	0x00556, 0x00ab5, 0x04ada, 0x006d2, 0x0c765, 0x00725, 0x0064b, 0x0a657, 0x00cab, 0x0055a, # 2021 ~ 2030
+	0x0656e, 0x00b69, 0x16f52, 0x00b52, 0x00b25, 0x0dd0b, 0x00a4b, 0x004ab, 0x0a2bb, 0x005ad, # 2031 ~ 2040
+	0x00b6a, 0x04daa, 0x00d92, 0x0eea5, 0x00d25, 0x00a55, 0x0ba4d, 0x004b6, 0x005b5, ]
+
+#    12~7         6~5    4~0
+# 离元旦多少天  春节月  春节日
+
+my_g_lunar_day = [
+	0x18d3, 0x1348, 0x0e3d, 0x1750, 0x1144, 0x0c39, 0x15cd, 0x1042, 0x0ab6, 0x144a, # 1901 ~ 1910
+	0x0ebe, 0x1852, 0x1246, 0x0cba, 0x164e, 0x10c3, 0x0b37, 0x14cb, 0x0fc1, 0x1954, # 1911 ~ 1920
+	0x1348, 0x0dbc, 0x1750, 0x11c5, 0x0bb8, 0x15cd, 0x1042, 0x0b37, 0x144a, 0x0ebe, # 1921 ~ 1930
+	0x17d1, 0x1246, 0x0cba, 0x164e, 0x1144, 0x0bb8, 0x14cb, 0x0f3f, 0x18d3, 0x1348, # 1931 ~ 1940
+	0x0d3b, 0x16cf, 0x11c5, 0x0c39, 0x15cd, 0x1042, 0x0ab6, 0x144a, 0x0e3d, 0x17d1, # 1941 ~ 1950
+	0x1246, 0x0d3b, 0x164e, 0x10c3, 0x0bb8, 0x154c, 0x0f3f, 0x1852, 0x1348, 0x0dbc, # 1951 ~ 1960
+	0x16cf, 0x11c5, 0x0c39, 0x15cd, 0x1042, 0x0a35, 0x13c9, 0x0ebe, 0x17d1, 0x1246, # 1961 ~ 1970
+	0x0d3b, 0x16cf, 0x10c3, 0x0b37, 0x14cb, 0x0f3f, 0x1852, 0x12c7, 0x0dbc, 0x1750, # 1971 ~ 1980
+	0x11c5, 0x0c39, 0x15cd, 0x1042, 0x1954, 0x13c9, 0x0e3d, 0x17d1, 0x1246, 0x0d3b, # 1981 ~ 1990
+	0x16cf, 0x1144, 0x0b37, 0x144a, 0x0f3f, 0x18d3, 0x12c7, 0x0dbc, 0x1750, 0x11c5, # 1991 ~ 2000
+	0x0bb8, 0x154c, 0x0fc1, 0x0ab6, 0x13c9, 0x0e3d, 0x1852, 0x12c7, 0x0cba, 0x164e, # 2001 ~ 2010
+	0x10c3, 0x0b37, 0x144a, 0x0f3f, 0x18d3, 0x1348, 0x0dbc, 0x1750, 0x11c5, 0x0c39, # 2011 ~ 2020
+	0x154c, 0x0fc1, 0x0ab6, 0x144a, 0x0e3d, 0x17d1, 0x1246, 0x0cba, 0x15cd, 0x10c3, # 2021 ~ 2030
+	0x0b37, 0x14cb, 0x0f3f, 0x18d3, 0x1348, 0x0dbc, 0x16cf, 0x1144, 0x0bb8, 0x154c, # 2031 ~ 2040
+	0x0fc1, 0x0ab6, 0x144a, 0x0ebe, 0x17d1, 0x1246, 0x0cba, 0x164e, 0x1042, 0x0b37, # 2041 ~ 2050
+	]
 
 #==================================================================================
 
 from datetime import date, datetime
-import calendar 
+import calendar
 
 START_YEAR = 1901
 
-LM_BIT = 12
-LM_DAY_BIT = 13
-LM_NUM_BIT = 14
+LM_DAY_BIT = 12
+LM_NUM_BIT = 13
 
 #　todo：正月初一 == 春节   腊月二十九/三十 == 除夕
 yuefeng = ["正月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "冬月", "腊月"]
-riqi = ["初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十", 
-    "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "廿十", 
+riqi = ["初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十",
+    "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "廿十",
     "廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "三十"]
 
 xingqi = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
@@ -61,16 +88,16 @@ shengxiao = ["鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡
 
 jieqi = [
     "小寒", "大寒",  # 1月
-    "立春", "雨水",  # 2月 
+    "立春", "雨水",  # 2月
     "惊蛰", "春分",  # 3月
     "清明", "谷雨",  # 4月
     "立夏", "小满",  # 5月
-    "芒种", "夏至",  # 6月 
+    "芒种", "夏至",  # 6月
     "小暑", "大暑",  # 7月
-    "立秋", "处暑",  # 8月 
-    "白露", "秋分",  # 9月 
+    "立秋", "处暑",  # 8月
+    "白露", "秋分",  # 9月
     "寒露", "霜降",  # 10月
-    "立冬", "小雪",  # 11月 
+    "立冬", "小雪",  # 11月
     "大雪", "冬至"]  # 12月
 
 def change_year(num):
@@ -108,27 +135,27 @@ def date_diff(tm):
 
 # 返回：
 # a b c
-# 闰几月，该月多少天 传入月份多少天
+# 闰几月，该闰月多少天 传入月份多少天
 def lunar_month_days(lunar_year, lunar_month):
     if (lunar_year < START_YEAR):
         return 30
 
     leap_month, leap_day, month_day = 0, 0, 0 # 闰几月，该月多少天 传入月份多少天
-    
+
     tmp = my_g_lunar_month_day[lunar_year - START_YEAR]
-    
+
     if tmp & (1<<(lunar_month-1)):
         month_day = 30
     else:
         month_day = 29
-    
+
     # 闰月
-    if (tmp & (1<<LM_BIT)):
+    leap_month = (tmp >> LM_NUM_BIT) & 0xf
+    if leap_month:
         if (tmp & (1<<LM_DAY_BIT)):
             leap_day = 30
         else:
             leap_day = 29
-        leap_month = (tmp >> LM_NUM_BIT) & 0xf
 
     #print("debug: year: %d month: %d (%d %d)0x%x" % (lunar_year, lunar_month, leap_day, month_day, my_g_lunar_month_day[lunar_year - START_YEAR]));
     return (leap_month, leap_day, month_day)
@@ -137,6 +164,7 @@ def lunar_month_days(lunar_year, lunar_month):
 def lunar_year_days(year):
     days = 0
     tmp = my_g_lunar_month_day[year - START_YEAR]
+
     # 1~12月
     for i in range(0, 12):
         if tmp & (1<<i):
@@ -144,19 +172,20 @@ def lunar_year_days(year):
         else:
             days += 29
     # 闰月
-    if (tmp & (1<<LM_BIT)):
+    leap_month = (tmp >> LM_NUM_BIT) & 0xf
+    if leap_month:
         if (tmp & (1<<LM_DAY_BIT)):
             lm_days = 30
         else:
             lm_days = 29
         days += lm_days
-        leap_month = (tmp >> LM_NUM_BIT) & 0xf
         #print("%d has leap month %d %d" % (year, leap_month, lm_days))
     #print("%d total day: %d" % (year, days))
     return days
 
 # 算农历日期
 # 返回的月份中，高4bit为闰月月份，低4bit为其它正常月份
+# 注：此函数每次计算均从1901年开始，较耗时
 def get_ludar_date(tm):
     span_days = date_diff(tm)
     #print("span_days %d" % span_days)
@@ -199,15 +228,94 @@ def get_ludar_date(tm):
     day += span_days
     return (year, month, day)
 
+# 算农历日期
+# 返回的月份中，高4bit为闰月月份，低4bit为其它正常月份
+# 注：新方式
+def get_ludar_date1(tm):
+    year, month, day = tm.year, 1, 1
+    code_data = my_g_lunar_day[year-1901]
+    days_tmp = (code_data >> 7) & 0x3f
+    chunjie_d = (code_data >> 0) & 0x1f
+    chunjie_m = (code_data >> 5) & 0x3
+    span_days = (tm - datetime(year, chunjie_m, chunjie_d)).days
+    #print("span_day: ", days_tmp, span_days, chunjie_m, chunjie_d)
+
+    # 日期在该年农历之后
+    if (span_days >= 0):
+        (leap_month, foo, tmp) = lunar_month_days(year, month)
+        while span_days >= tmp:
+            span_days -= tmp
+            if (month == leap_month):
+                (leap_month, tmp, foo) = lunar_month_days(year, month)
+                if (span_days < tmp): # 指定日期在闰月中
+                    month = (leap_month<<4) | month
+                    break
+                span_days -= tmp
+            month += 1 # 此处累加得到当前是第几个月
+            (leap_month, foo, tmp) = lunar_month_days(year, month)
+        day += span_days
+        #print("----m, d ", month&0xf, day)
+        return year, month, day
+    # 倒算日历
+    else:
+        month = 12
+        year -= 1
+        (leap_month, foo, tmp) = lunar_month_days(year, month)
+        #print("++ ", month, leap_month, span_days, tmp)
+        while abs(span_days) >= tmp:
+            span_days += tmp
+            month -= 1
+            #print("-- ", month, leap_month, span_days, tmp)
+            if (month == leap_month): # tocheck
+                #print("  -- ", leap_month)
+                (leap_month, tmp, foo) = lunar_month_days(year, month)
+                if (abs(span_days) < tmp): # 指定日期在闰月中
+                    month = (leap_month<<4) | month
+                    break
+                span_days += tmp
+            (leap_month, foo, tmp) = lunar_month_days(year, month)
+        day += (tmp + span_days) # 从月份总数中倒扣 得到天数
+        return year, month, day
+
+def show_month1(tm):
+    (ly, lm, ld) = get_ludar_date1(tm)
+    print("%d年%d月%d日" % (tm.year, tm.month, tm.day), week_str(tm), end='')
+    print("\t农历 %s年 %s年%s%s " % (y_lunar(ly), change_year(ly), m_lunar(lm), d_lunar(ld))) # 根据数组索引确定
+    print("一\t二\t三\t四\t五\t六\t日")
+
+    c = calendar.Calendar(0)
+    ds = [d for d in c.itermonthdays(tm.year, tm.month)]
+
+    #print(len(ds), ds)
+    # 利用calendar直接获取指定年月日期
+    count = 0
+    for d in ds:
+        if d == 0:
+            print("\t", end='')
+            count += 1
+            continue
+
+        (ly, lm, ld) = get_ludar_date(datetime(tm.year, tm.month, d))
+
+        if count % 7 == 0:
+            print("\n", end='')
+        d_str = str(d)
+        if d == tm.day:
+            d_str = "*" + d_str
+        print("%s\t" % (d_str + d_lunar1(lm, ld)), end='')
+        #print("%s\t" % (d_str), end='')
+        count += 1
+    print("")
+
 def show_month(tm):
     (ly, lm, ld) = get_ludar_date(tm)
     print("%d年%d月%d日" % (tm.year, tm.month, tm.day), week_str(tm), end='')
     print("\t农历 %s年 %s年%s%s " % (y_lunar(ly), change_year(ly), m_lunar(lm), d_lunar(ld))) # 根据数组索引确定
     print("一\t二\t三\t四\t五\t六\t日")
- 
+
     c = calendar.Calendar(0)
     ds = [d for d in c.itermonthdays(tm.year, tm.month)]
-    
+
     #print(len(ds), ds)
     # 利用calendar直接获取指定年月日期
     count = 0
@@ -231,7 +339,7 @@ def show_month(tm):
 
 def _show_month(year, month, day):
     tmp = datetime(year, month, day)
-    show_month(tmp)
+    show_month1(tmp)
     print('==========================================')
 
 def this_month():
@@ -239,8 +347,14 @@ def this_month():
     #print('--------------------------')
     _show_month(datetime.now().year, datetime.now().month, datetime.now().day)
 
-this_month()
+_show_month(1962, 2, 4)
+_show_month(1934, 1, 1)
+_show_month(2033, 12, 27)
 _show_month(2034, 1, 1)
-_show_month(2047, 6, 1)
+_show_month(2034, 2, 1)
+_show_month(2034, 3, 1)
+#this_month()
+#_show_month(2029, 3, 1)
+#_show_month(2047, 6, 1)
 #_show_month(1903, 4, 1)
 
