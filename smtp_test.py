4abdef359962e9ce431d935b93f1f7d37b26d10c
Late Lee <li@latelee.org>
2021-01-25 23:31:20 +0800

    update smtp_test.py

--- smtp_test.py
+++ smtp_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # encoding: utf-8
+# 文件名：smtp_test.py
 # 基于python 3.4 发送邮件测试示例，本代码文件使用UTF-8格式
 # 隐藏账户信息
 
@@ -11,14 +12,14 @@ import email.utils
 from email.mime.text import MIMEText
 
 # 接收邮件地址
-to_email = 'latelee@163.com'
+to_email = 'latelee@163.com, li@latelee.org'
 #to_email = str.splitfields(to_email, ",")
 
 # 发送者信(最好是马甲邮箱)
-smtpserver = 'smtp.163.com'
-snd_email = 'rtl8019ads@163.com'
+smtpserver = 'smtp.exmail.qq.com'
+snd_email = 'test@latelee.org'
 username = snd_email
-password = b'ccccccnRsceeggO3DAxOWFzPeTEwMA=='
+password = b'1qaz@WSX'
 
 
 subject = 'python email test'
@@ -34,7 +35,8 @@ def send_email(to_list, sub, content):
     try:
         smtp = smtplib.SMTP()
         smtp.connect(smtpserver)
-        smtp.login(username, bytes.decode(base64.b64decode(password)))
+        #smtp.login(username, bytes.decode(base64.b64decode(password)))
+        smtp.login(username, bytes.decode(password))
         smtp.sendmail(snd_email, to_list, msg.as_string())
         smtp.quit()
         return 0
@@ -44,7 +46,7 @@ def send_email(to_list, sub, content):
 
 # main...
 if __name__ == '__main__':  
-    if send_email(to_email, "hello", "hello world") == 0:  
+    if send_email(to_email, "hello", "hello world, this is a python email test") == 0:  
         print("send %s ok" % to_email)
     else:  
         print("send failed")
